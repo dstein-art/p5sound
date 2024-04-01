b438fe647f0b4f373fa2d921f01506481f3028b8
dstein <steinster@gmail.com>
2024-03-31 20:28:43 -0400

    Added Columns

--- lessons/p5basics-5.js
+++ lessons/p5basics-5.js
@@ -1,10 +1,10 @@
 function setup() {
     createCanvas(100, 100);
-  }
-  
-  function draw() {
+}
+
+function draw() {
     background(220);
     text("move mouse",5,80);
     text("over canvas",5,90);
     square(mouseX,mouseY,30);
-  }
+}
